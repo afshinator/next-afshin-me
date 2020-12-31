ace3cebc4551bac9134b328e18ed62a2604a1eda
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-30 21:06:06 -0800

    Add Chakra UI and its goodies and wrap app in page transtion

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
+- - -
 
 Added TypeScript following https://nextjs.org/docs/basic-features/typescript
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { CSSReset, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import customTheme from "./../styles/theme";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Pulling in custom font primarily using this example: https://chakra-ui.com/guides/with-nextjs-font-files
+// but pre-loading them in the layout as per https://kirazhang.com/posts/nextjs-custom-fonts
+
+const theme = extendTheme(customTheme);
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <motion.div
+        key={router.route} // presence of this key enables the animation
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: { opacity: 0 },
+          pageAnimate: { opacity: 1 },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
