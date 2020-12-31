7103c27266a81af35aa5557f9f668831d9c4f1fe
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-30 20:54:42 -0800

    Add some needed dirs

---
+++ styles/theme.js
@@ -0,0 +1,22 @@
+
+const customTheme = {
+  // styles,
+  // borders,
+  // components: {
+  //   Button,
+  // },
+  colors: {
+    my: {
+      100: "#d69e2e",
+      // ...
+      900: "#33658a",
+    },
+  },
+  fonts: {
+    heading: "'BarlowCondensed',Helvetica,Arial,sans-serif"
+  }
+}
+
+
+
+export default customTheme
